3c3aaf9f4ef60123ae13324174c3a110eae23131
Brandon Weaver <a7xncob@gmail.com>
2020-02-28 19:04:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function map(array, func) {
 }
 
 function reduce(array, func, start) {
-  let returnValue = !!start ?;
+  let returnValue = !!start ? start : array[0];
   for (let i = start > 0 ? 1 : 0; i < array.length; i++) {
     returnValue = func(array[i], returnValue);
   }
