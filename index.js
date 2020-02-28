5fb526dbab693057ce9746af3155381ca2d64af4
Brandon Weaver <a7xncob@gmail.com>
2020-02-28 19:06:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your functions here
+function map(array, func) {
+  let newArray = [];
+  for (const element of array) {
+    newArray.push(func(element));
+  }
+  return newArray;
+}
+
+function reduce(array, func, start) {
+  let returnValue = !!start ? start : array[0];
+  for (let i = !!start ? 0 : 1; i < array.length; i++) {
+    returnValue = func(array[i], returnValue);
+  }
+  return returnValue;
+}
